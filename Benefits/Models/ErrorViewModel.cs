815b57c5087f29c16fc44ec8015151ebff4f61ba
Carlos <carlos.oli1505@gmail.com>
2019-10-26 18:02:15 -0300

    Criação do projeto com padrão MVC. E inserido tema bootswatch para estilização.

---
+++ Benefits/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Benefits.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
