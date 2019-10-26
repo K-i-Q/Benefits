2b7ebb13f98f621c6e5fb94af6f553620076f678
Carlos <carlos.oli1505@gmail.com>
2019-10-26 20:29:34 -0300

    Instalação do EF e adição da configuração da String de Conexão
    Instalação do EF e adição da configuração da String de Conexão

--- Benefits/Startup.cs
+++ Benefits/Startup.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Benefits.DAL;
+using Benefits.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,7 +34,10 @@ namespace Benefits
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddDbContext<Context>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("BenefitsConnection")
+                ));
+            services.AddScoped<ClienteDAO>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
