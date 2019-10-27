48544a0a42b3267f216d7a6c02471b47dbe7a372
Carlos <carlos.oli1505@gmail.com>
2019-10-27 01:22:59 -0300

    Modelo Empresa
    Criado classe de modelo da Empresa

---
+++ Benefits/Models/Empresa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benefits.Models
+{
+    [Table("Empresas")]
+    public class Empresa
+    {
+        [Key]
+        public long EmpresaId { get; set; }
+        public string Cnpj { get; set; }
+        public string Razao { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Telefone { get; set; }
+        public DateTime CriadaEm { get; set; }
+
+        public Empresa()
+        {
+            CriadaEm = DateTime.Now;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Empresa empresa = (Empresa)obj;
+            return empresa.Cnpj == Cnpj;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[CNPJ > " + Cnpj);
+            sb.Append(", Razão > " + Razao);
+            sb.Append(", Email > " + Email);
+            sb.Append(", Telefone > " + Telefone);
+            sb.Append(", Criada em > " + CriadaEm+"]");
+            return sb.ToString();
+        }
+    }
+}
