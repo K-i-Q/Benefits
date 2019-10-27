4fcaa7e1e1fc39f54244fa807c869ac564bdf826
Carlos <carlos.oli1505@gmail.com>
2019-10-27 01:25:18 -0300

    Classe EmpresaDAO
    Criado classe EmpresaDAO

---
+++ Benefits/DAL/EmpresaDAO.cs
@@ -0,0 +1,19 @@
+﻿using Benefits.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Benefits.DAL
+{
+    public class EmpresaDAO
+    {
+        private readonly Context _context;
+
+        public EmpresaDAO(Context context)
+        {
+            _context = context;
+        }
+
+    }
+}
