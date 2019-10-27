096f12913cfdf32df12b2859f5e17d7c6b8ea98d
Carlos <carlos.oli1505@gmail.com>
2019-10-27 13:15:38 -0300

    View Login Empresa
    Criado View para Login da Empresa e adicionado Action para redirecionamento até a View, dentro da LoginController

--- Benefits/Controllers/LoginController.cs
+++ Benefits/Controllers/LoginController.cs
@@ -8,7 +8,15 @@ namespace Benefits.Controllers
 {
     public class LoginController : Controller
     {
+        //TODO: Autenticação do cliente
+        //TODO: Autenticação da empresa
+
         public IActionResult LoginCliente()
+        {
+            return View();
+        }
+
+        public IActionResult LoginEmpresa()
         {
             return View();
         }
